36db1871af2ff82204336e3d123725ebf1dd79a1
alexlo0612 <alexlo0612@hotmail.com.tw>
2018-03-16 20:15:57 +0800

    Date Input Update

--- Econ.py
+++ Econ.py
@@ -18,29 +18,52 @@ x = datetime.datetime.now().year
 y = datetime.datetime.now().month
 
 #-------------------------Define Var. For Current Date and Target Date-------------------------
-d0 = datetime.date.today()
-ds = datetime.date(2017,12,13)
-df = datetime.date(2017,12,27)
+d0 = datetime.datetime.today() #Today's Date
+
+def GetDatefunc():
+
+        UserIn = input("Start Date (DD/MM/YYYY):")
+        try:
+            ds = datetime.datetime.strptime(UserIn, "%d/%m/%Y")
+        except Exception as e:
+            print ("ops watch the format!")
+        else:
+            return ds
+
+def GetDatefunc2():
+        UserIn2 = input("End Date (DD/MM/YYYY):")
+        try:
+            df = datetime.datetime.strptime(UserIn2, "%d/%m/%Y")
+        except Exception as e:
+            print ("ops watch the format!")
+        else:
+            return df
+
+ds = GetDatefunc()
+df = GetDatefunc2()
+
+
+
+#ds = datetime.date(2017,12,13)
+#df = datetime.date(2017,12,27)
 #tmr = datetime.date.today() + datetime.timedelta(days=1)
 
 #-------------------------Calculate Differences-------------------------
 dealta = df - d0
 dealtac = df - ds
-dealc = d0 - ds 
+dealc = d0 - ds
 
 #-------------------------Define Page Number and Calculate Average Page per Day-------------------------
 Page = 300.0
 PageLeftc = Page / dealtac.days
 PageLeft = Page - dealc.days * PageLeftc
+
 #PageLeft = Page - dealta.days * PageLeftc
 #PageLeft = Page - dealta.days * int(PageLeftc)
 #round (Page / dealta.days, 2)
 
 #-------------------------Print Outputs-------------------------
 print(calendar.month(x,y))
-print "Time till Exam:",dealta.days
-print "Page Per Day:", round(PageLeftc,2)
-print "Page Left:", PageLeft
-
-
-
+print ("Time till Exam:", dealta.days)
+print ("Page Per Day:", round(PageLeftc,2))
+print ("Page Left:", PageLeft)
