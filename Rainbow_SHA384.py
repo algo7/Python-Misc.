0d009f83909284b6a2d078bda5f536c16925c6bb
alexlo0612 <alexlo0612@hotmail.com.tw>
2018-03-26 15:20:35 +0800

    Add Scripts for Other Hashing Algorithms

---
+++ Rainbow_SHA384.py
@@ -0,0 +1,38 @@
+#-----------------Libraries-------------------------
+###System I.O Lib. Need to be Added for Later Usage
+import hashlib
+
+#-----------------Variables-------------------------
+
+##Var. for Input File (No New Line -> tr -d '\n')
+###Additional Code Required for Asking for User's Input
+infile = '8H'
+
+##Open Filestream (infile) / Append Mode
+###Additional Code Required for Asking for User's Input
+outfile = open("MD.txt","a")
+
+#------------------SHA384_Hashing_Algorithm--------------
+##Open Filestream (outfile) / Readonly Mode
+with open(infile, "r") as inf:
+    ##For Loop -> Read the File Line-by-Line
+    for line in inf:
+
+        ##Write SHA384 Hash Digest to the Output/ Strip(\n) / Ecnode(for Py3.x) / Newline
+        outfile.write(hashlib.sha384(line.strip().encode('utf-8')).hexdigest()+'\n')
+
+
+
+
+
+
+
+
+
+
+
+
+######
+
+#print(line, end="")
+#print (hashlib.md5(line.strip().encode('utf-8')).hexdigest())
