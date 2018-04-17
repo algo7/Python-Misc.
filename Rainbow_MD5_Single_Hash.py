7462e4031d9313edda5758f755e3ba3b2548bb21
alexlo0612 <alexlo0612@hotmail.com.tw>
2018-04-17 23:18:50 +0800

    Single Hash Code for Comparison with C

---
+++ Rainbow_MD5_Single_Hash.py
@@ -0,0 +1,22 @@
+#-----------------Libraries-------------------------
+###System I.O Lib. Need to be Added for Later Usage
+import hashlib
+
+#-----------------Variables-------------------------
+print (hashlib.md5(b"a").hexdigest())
+
+
+
+
+
+
+
+
+
+
+
+
+######
+
+#print(line, end="")
+#print (hashlib.md5(line.strip().encode('utf-8')).hexdigest())
